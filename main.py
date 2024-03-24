24154985163d98cabd4e2aea41b33d34eeb56fbe
Diya <diya.solanki.31@gmail.com>
2024-03-24 20:06:23 +0530

    Basic API EndPoint to extract data

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import json
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -15,3 +19,15 @@ df_protections = pd.json_normalize(data['protections'])
 result = pd.concat([df_protections, df_schedules], axis=1)
 
 print(result)
+
+@app.route('/data/<string:id>', methods=['GET'])
+def get_data_by_id(id):
+    filtered_data = result[result['id'] == id]
+    if filtered_data.empty:
+        return jsonify({'error': 'No data found for the given ID'}), 404
+    else:
+        return jsonify(filtered_data.to_dict(orient='records'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
