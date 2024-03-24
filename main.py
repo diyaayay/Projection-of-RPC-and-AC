2315ed56670af2f97500a89ae4c3983e970d32b4
Diya <diya.solanki.31@gmail.com>
2024-03-24 12:53:10 +0530

    normalized schedules for time

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
 with open("sampleJson.json", "r") as data_file:
-    data=json.load(data_file)
+    data = json.load(data_file)
+
+
+df_schedules = pd.json_normalize(data['protections'], 'schedules', errors='ignore')
+
 
 df_protections = pd.json_normalize(data['protections'])
-print(df_protections)
+result = pd.concat([df_protections, df_schedules], axis=1)
+
+print(result)
