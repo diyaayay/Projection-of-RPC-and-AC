15d418e29133e89adf796c9fcbd1ac39cd3e077d
Diya <diya.solanki.31@gmail.com>
2024-05-24 13:55:48 +0530

    Removing code not in use anymore post merge

--- backend/getOverlaps.py
+++ backend/getOverlaps.py
@@ -2,12 +2,10 @@ from collections import OrderedDict
 import json
 from math import gcd
 from datetime import datetime, timedelta
-
-# import backend.policyTree as policyTree
-import policyTree
+import backend.policyTree as policyTree
 
 # Reading the data from the json for now. Later should be directly read from the server
-with open("json_files/sample_json_2.json") as data_file:
+with open("json_files/sample_json_1.json") as data_file:
     req_payload = json.load(data_file)
 
 
@@ -21,32 +19,41 @@ paths = policyTree.find_all_paths(root)
 # print(paths)
 
 
-def find_recent_source(path, initial_times, intervals, end_time):
-    print(path)
-    print(initial_times)
-    print(intervals)
+# function that gives all the overlaps possible for given schedules
+def find_simultaneous_ring(path, initial_times, intervals, end_time):
     time_format = "%Y-%m-%d %H:%M"
-    times = [datetime.strptime(time_str, time_format) for time_str in initial_times]
     end_time = datetime.strptime(end_time, time_format)
-    while all(time < end_time for time in times):
-        min_time = min(times)
-        min_indices = [index for index, value in enumerate(times) if value == min_time]
+    times = [datetime.strptime(time_str, time_format) for time_str in initial_times]
+    occurrences = []
 
+    while all(time < end_time for time in times):
+        if all(time == times[0] for time in times):
+            tup = [
+                (time.strftime(time_format) + " " + str(path[idx]))
+                for idx, time in enumerate(times)
+            ]
+            occurrences.append(tuple(tup))
+        else:
+            for i in range(len(times)):
+                for j in range(i + 1, len(times)):
+                    if times[i] == times[j] and j - i == 1:
+                        if all(times[k] >= times[i] for k in range(i)):
+                            occurrences.append(
+                                (
+                                    times[i].strftime(time_format) + " " + str(path[i]),
+                                    times[j].strftime(time_format) + " " + str(path[j]),
+                                )
+                            )
+
+        min_time = min(times)
+        min_indices = [i for i, time in enumerate(times) if time == min_time]
         for idx in min_indices:
-            if path[idx] != 1:  # not snapshot
-                # print the link present, either cloud backup or on-prem
-                source_time = times[idx - 1] - timedelta(hours=intervals[idx - 1])
-                print(
-                    f"source : {source_time.strftime(time_format)} [{idx-1}] -> current : {times[idx].strftime(time_format)} [{idx}]"
-                )
-                times[idx] += timedelta(hours=intervals[idx])
-            else:
-                print(
-                    f"current : {times[idx].strftime(time_format)} [{idx}]"
-                )
-                times[idx] += timedelta(hours=intervals[idx])
-    
-    return []
+            times[idx] += timedelta(hours=intervals[idx])
+
+    if not occurrences:
+        return "No occurrences found."
+    return list(OrderedDict.fromkeys(occurrences))
+
 
 # for all the valid paths we are going to find the overlaps
 def get_res(endTime):
@@ -57,21 +64,15 @@ def get_res(endTime):
         for id in path[1:]:
             node = policyTree.find_node(root, id)
             initial_times.append(
-                req_payload["createdAt"].split(" ")[0] + " " + node.startTime
+                req_payload["createdAt"].split("T")[0] + " " + node.startTime
             )
             intervals.append(node.interval)
         res.append(
             {
                 "schedules_involved": path[1:],
-                "occurrences": find_recent_source(
+                "occurrences": find_simultaneous_ring(
                     path[1:], initial_times, intervals, endTime
                 ),
             }
         )
-        # print("---------------------")
-        print(res)
     return res
-
-if __name__ == "__main__":
-    # get_res("2024-04-10 11:12")
-    get_res("2023-10-26 11:12")
--- backend/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-res.append(
-        #     {
-        #         "schedules_involved": path[1:],
-        #         "occurrences": find_recent_source(path[1:], endTime),
-        #     }
-        # )
